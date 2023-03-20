a7e5f204f63bbe83f520b2164aa888636cb8579b
Jan Metzger <jan.metzger@mailbox.org>
2023-03-20 15:48:02 +0100

    Fix injector error

--- client/src/legacy/TinyMCE_sslink-elemental.js
+++ client/src/legacy/TinyMCE_sslink-elemental.js
@@ -34,7 +34,7 @@ const plugin = {
 const modalId = 'insert-link__dialog-wrapper--elemental';
 const sectionConfigKey = 'SilverStripe\\CMS\\Controllers\\CMSPageEditController';
 const formName = 'editorElementalLink';
-const InsertLinkElementalModal = provideInjector(createInsertLinkModal(sectionConfigKey, formName));
+const InsertLinkElementalModal = loadComponent(createInsertLinkModal(sectionConfigKey, formName));
 
 jQuery.entwine('ss', ($) => {
   $('textarea.htmleditor').entwine({
